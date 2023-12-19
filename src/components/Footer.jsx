ae086316cd52303e2f8134b32cf649bd11cb8742
Tushar prajapati <tushargola958@gmail.com>
2023-12-19 17:30:37 +0530

    logo updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/footer.css'
-import image from '../assets/img/logo.png'
+import image from '../assets/logo.png'
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -14,7 +14,7 @@ const Footer = () => {
 <a href="#"><i className="fa-brands fa-facebook face"></i></a>
 <a href="tel:9582124881"><i className="fa-solid fa-phone phone"></i></a>
 </div>
-<img src={image} alt="" id='image'/>
+<Link to='/'><img src={image} alt="" id='image'/></Link>
 <div className="row">
 <ul>
 <Link to='/'> <li><a href="#">Home</a></li></Link>
